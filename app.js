0c36b3c1db886236cbdc46522e1dabdbf6a4caf1
Matthew O'Connell <matthew.j.oconnell1@gmail.com>
2014-05-15 12:11:53 -0400

    app.js updated to handle routes

--- app.js
+++ app.js
@@ -2,17 +2,29 @@
 var express = require('express'),
     http = require('http'),
     path = require('path'),
-    routes = require('./app/routes'),
+    bodyParser = require('body-parser'),
+    cookieParser = require('cookie-parser'),
+    methodOverride = require('method-override'),
+    routes = require('./config/routes'),
+    logger = require('morgan'),
     mongoose = require('mongoose'),
+    configDB = require('./config/database'),
     app = express();
 
 // Initialize Database
-var configDB = require('./config/database');
 mongoose.connect(configDB.url);
 
 app.set('port', process.env.PORT || 3000);
-app.set('views', __dirname + '/app/views');
+app.set('views', path.join(__dirname, 'app/views'));
 app.set('view engine', 'jade');
 
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(cookieParser());
+app.use(methodOverride());
+app.use('/', express.static(path.join(__dirname, 'public')));
+
+routes.initialize(app)
 
 app.listen(process.env.PORT || 3000);
